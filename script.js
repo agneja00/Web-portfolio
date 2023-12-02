1a9be53f0ad0b1061a4a62780f4f5a9f567ea81f
agneja00 <agnieshaa00@gmail.com>
2023-12-02 16:16:20 +0200

    portfolio done

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector('.header__menu');
+let menuLinks = document.querySelector('.header__links');
+
+menuBtn.addEventListener('click', function () {
+	menuBtn.classList.toggle('active');
+	menuLinks.classList.toggle('active');
+})
