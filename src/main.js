e4b5fb009eab0401334e5499720f512e1daadf44
agneja00 <agnieshaa00@gmail.com>
2025-11-24 08:02:19 +0200

    Fixed bug with window resize and header links

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import "./sass/style.scss";
 let menuBtn = document.querySelector(".header__menu");
 let menuLinks = document.querySelector(".header__links");
 
-menuBtn.addEventListener("click", function () {
+menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   menuLinks.classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    menuBtn.classList.remove("active");
+    menuLinks.classList.remove("active");
+  }
 });
